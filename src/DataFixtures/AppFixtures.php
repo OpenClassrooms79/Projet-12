93f95fe732728681259b6f57ab49d849eee5df7a
Jocelyn <J0celyn@users.noreply.github.com>
2025-02-12 03:05:01 +0100

    fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Advice;
+use App\Entity\Month;
+use App\Entity\User;
+use App\Repository\MonthRepository;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker;
+
+use function random_int;
+
+class AppFixtures extends Fixture
+{
+    public const NB_USERS = 20;
+    public const NB_ADVICES = 50;
+    public const NB_MONTHS = 12;
+    private array $months = [];
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Faker\Factory::create();
+
+        $this->loadUsers($manager, $faker);
+        $this->loadMonths($manager);
+        $this->loadAdvices($manager, $faker);
+    }
+
+    public function loadUsers(ObjectManager $manager, Faker\Generator $faker): void
+    {
+        for ($i = 1; $i <= self::NB_USERS; $i++) {
+            $user = new User();
+            $user
+                ->setLogin($faker->userName())
+                ->setPassword($faker->password())
+                ->setPostCode($faker->postcode());
+            $manager->persist($user);
+        }
+        $manager->flush();
+    }
+
+    public function loadMonths(ObjectManager $manager): void
+    {
+        $months = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
+        for ($i = 0; $i < self::NB_MONTHS; $i++) {
+            $month = new Month();
+            $month
+                ->setNum($i + 1)
+                ->setName($months[$i]);
+            $manager->persist($month);
+        }
+        $manager->flush();
+
+        $this->months = $manager->getRepository(Month::class)->findAll();
+    }
+
+    public function loadAdvices(ObjectManager $manager, Faker\Generator $faker): void
+    {
+        for ($i = 1; $i <= self::NB_ADVICES; $i++) {
+            $advice = new Advice();
+            $advice->setDetail($faker->text(300));
+
+            for ($n = 1; $n < random_int(1, self::NB_MONTHS); $n++) {
+                $advice->addMonth($this->months[array_rand($this->months)]);
+            }
+
+            $manager->persist($advice);
+        }
+        $manager->flush();
+    }
+}
