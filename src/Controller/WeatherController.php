642971ab480de6a42f7c34e20643a5341f50927c
Jocelyn <J0celyn@users.noreply.github.com>
2025-02-18 18:53:15 +0100

    factorisation du code correction d'erreurs

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Geo;
 use App\Repository\GeoRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Console\Command\Command;
@@ -19,21 +20,55 @@ use const JSON_THROW_ON_ERROR;
 
 final class WeatherController extends AbstractController
 {
+    public function __construct(private GeoRepository $geoRepository, private TranslatorInterface $translator, private EntityManagerInterface $entityManager) {}
+
+    #[Route('/api/meteo', name: 'app_weather_city_default', methods: ['GET'])]
+    public function show(UserRepository $userRepository): Response
+    {
+        // TODO récupérer la ville de l'utilisateur courant
+        $user = $userRepository->findOneBy(['id' => 15]);
+        if ($user === null) {
+            return $this->json([
+                'code' => 404,
+                'message' => 'Utilisateur non trouvé',
+            ]);
+        }
+
+        return $this->getWeatherFromCity($user->getCity());
+    }
+
     #[Route('/api/meteo/{city}', name: 'app_weather_city', methods: ['GET'])]
-    public function index(string $city, GeoRepository $geoRepository, TranslatorInterface $translator, EntityManagerInterface $entityManager): Response
+    public function show2(string $city): Response
+    {
+        return $this->getWeatherFromCity($city);
+    }
+
+    protected function getWeatherFromCity(string $city): Response
+    {
+        $geo = $this->getCoordinatesFromCity($city);
+        if ($geo === null) {
+            return $this->json([
+                'code' => 404,
+                'message' => 'Ville non trouvée',
+            ]);
+        }
+
+        $weather_result = $this->getWeatherFromGeo($geo);
+        return $this->json([
+            'code' => 200,
+            'message' => $weather_result,
+        ]);
+    }
+
+    protected function getCoordinatesFromCity(string $city): ?Geo
     {
         // vérifier si les infos ne sont pas déjà en cache
-        $geo = $geoRepository->findOneBy(['name' => $city]);
-        if ($geo !== null) {
-            $geo_result = [
-                'lat' => $geo->getLatitude(),
-                'lon' => $geo->getLongitude(),
-            ];
-        } else {
+        $geo = $this->geoRepository->findOneBy(['name' => $city]);
+        if ($geo === null) {
             try {
                 $geo_result = json_decode(
                     file_get_contents(
-                        $translator->trans(
+                        $this->translator->trans(
                             $_ENV['GEO_COORDINATES_URL'],
                             [
                                 '{API_KEY}' => $_ENV['OPENWEATHERMAP_API_KEY'],
@@ -45,33 +80,33 @@ final class WeatherController extends AbstractController
                     512,
                     JSON_THROW_ON_ERROR,
                 )[0];
+                // mettre en cache les infos
+                $geo = new Geo();
+                $geo->setCountryCode($geo_result['country']);
+                $geo->setName($geo_result['name']);
+                $geo->setLatitude($geo_result['lat']);
+                $geo->setLongitude($geo_result['lon']);
+                $this->entityManager->persist($geo);
+                $this->entityManager->flush();
             } catch (\Exception $e) {
-                return $this->json([
-                    'code' => 500,
-                    'message' => 'Erreur 1 du serveur',
-                ], 500);
+                return null;
             }
-
-            // mettre en cache les infos
-            $geo = new Geo();
-            $geo->setCountryCode($geo_result['country']);
-            $geo->setName($geo_result['name']);
-            $geo->setLatitude($geo_result['lat']);
-            $geo->setLongitude($geo_result['lon']);
-            $entityManager->persist($geo);
-            $entityManager->flush();
         }
 
-        // récupération de la météo
+        return $geo;
+    }
+
+    protected function getWeatherFromGeo(Geo $geo): ?array
+    {
         try {
-            $weather_result = json_decode(
+            return json_decode(
                 file_get_contents(
-                    $translator->trans(
+                    $this->translator->trans(
                         $_ENV['CURRENT_WEATHER_URL'],
                         [
                             '{API_KEY}' => $_ENV['OPENWEATHERMAP_API_KEY'],
-                            '{LATITUDE}' => $geo_result['lat'],
-                            '{LONGITUDE}' => $geo_result['lon'],
+                            '{LATITUDE}' => $geo->getLatitude(),
+                            '{LONGITUDE}' => $geo->getLongitude(),
                             '{LANGUAGE_CODE}' => 'FR',
                         ],
                     ),
@@ -81,15 +116,7 @@ final class WeatherController extends AbstractController
                 JSON_THROW_ON_ERROR,
             );
         } catch (\Exception $e) {
-            return $this->json([
-                'code' => 500,
-                'message' => $e->getMessage(),
-            ], 500);
+            return null;
         }
-
-        return $this->json([
-            'code' => 200,
-            'message' => $weather_result,
-        ]);
     }
 }
