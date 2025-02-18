a861df2175292ca47f3c3cd3b4da5d0c6b1b6101
Jocelyn <J0celyn@users.noreply.github.com>
2025-02-18 21:29:45 +0100

    amélioration de la documentation pour les routes relatives à l'entité Advice

--- src/Entity/Advice.php
+++ src/Entity/Advice.php
@@ -3,13 +3,63 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
+use App\Controller\AdviceController;
 use App\Repository\AdviceRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: AdviceRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    description: 'test de description globale',
+    operations: [
+        new Get(
+            uriTemplate: '/api/conseil',
+            routeName: 'advice_current_month',
+            controller: AdviceController::class,
+            description: 'test de description pour une route spécifique',
+            extraProperties: [
+                'openapi' => [
+                    'summary' => 'Récupération d\'une entité',
+                    'description' => 'Permet de récupérer une entité spécifique par son identifiant.',
+                ],
+            ],
+        ),
+        new Get(
+            uriTemplate: '/api/conseil/{month}',
+            routeName: 'advice_specific_month',
+            controller: AdviceController::class,
+        ),
+        new Post(
+            uriTemplate: '/api/conseil/{months}/{detail}',
+            routeName: 'advice_add',
+            controller: AdviceController::class,
+            description: 'Ajoute un nouveau conseil',
+        ),
+        new Put(
+            uriTemplate: '/api/conseil/{id}/{months}',
+            routeName: 'advice_update',
+            controller: AdviceController::class,
+            description: 'Met à jour la liste des mois du conseil $id',
+        ),
+        new Put(
+            uriTemplate: '/api/conseil/{id}/{months}/{detail}',
+            routeName: 'advice_update2',
+            controller: AdviceController::class,
+            description: 'Met à jour la liste des mois du conseil $id, et éventuellement le détail',
+        ),
+        new Delete(
+            uriTemplate: '/api/conseil/{id}',
+            routeName: 'advice_delete',
+            controller: AdviceController::class,
+            description: 'Supprime le conseil $id',
+        ),
+    ]
+)]
 class Advice
 {
     #[ORM\Id]
